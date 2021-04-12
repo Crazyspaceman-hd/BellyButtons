62db7f3854cc50b60afbe166996b9cc6ff528fa3
unknown <hvdeane@gmail.com>
2021-04-11 17:37:17 -0700

    Been very frustrated, finally feels like making headway.

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,51 @@
 d3.json('samples.json').then(data => {
     console.log(data);
+    let samples = data.samples;
+    console.log(samples);
+    d3.select("#selDataset")
+        .selectAll("option")
+        .data(data.samples)
+        .join("option")
+        .attr("value", data => data.id)
+        .text(data => data.id)
+    })
+
+// d3.json('samples.json').then(data => {
+//     let samples = data.samples;
+//     const filterSub = (samples) => samples.id == inputValue;
+//     let filteredSub = samples.filter(filterSub);
+//     console.log(filteredSub);
+// })
+
+
+function optionChanged(inputValue) {
+  d3.json('samples.json').then(data => {
+    let samples = data.samples;
+    const filterSub = (samples) => samples.id == inputValue;
+    let filteredSub = samples.filter(filterSub);
+    console.log(filteredSub);
 })
+}
+const placeholder= 940
+
+
+
+// var subject =(d3.json('samples.json')).filter(filterSubject)
+
+// d3.json('samples.json').then(function(data) {
+//      let 
+let form=d3.select("#selDataset")
+
+
+
+
+
+
+
+    function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }
+
+
